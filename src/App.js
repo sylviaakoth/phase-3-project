4fa5a41deea115e585e934987b598304beabb62a
sylviaakoth <109962507+sylviaakoth@users.noreply.github.com>
2022-11-09 15:02:13 +0300

    imported julius code directly cause of conflicts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter ,Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Support from "./pages/support";
+import Caption from "./components/Caption";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Group 4 project.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <BrowserRouter>
+      
+        <Navbar />
+       
+      <Routes>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/support" component={Support} />
+        
+      </Routes>
+       
+
+      
+    </BrowserRouter>
+  
+  <Caption/>
+
+  </div>
+    
   );
 }
-
 export default App;
